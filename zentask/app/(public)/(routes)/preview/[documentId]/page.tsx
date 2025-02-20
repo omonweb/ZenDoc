1c1986e0a1ea013a46dc2e7d444909d4e1d3ca22
om arora <om30arora@gmail.com>
2025-02-20 06:55:19 +0530

    preview-bg-test-7-final

--- zentask/app/(public)/(routes)/preview/[documentId]/page.tsx
+++ zentask/app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -32,7 +32,7 @@ const DocumentIdPage = () => {
 
     if (document === undefined) {
         return (
-            <div>
+            <div className="bg-[#1F1F1F] min-h-screen">
                 <Cover.Skeleton />
                 <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
                     <div className="space-y-4 pl-8 pt-4">
@@ -53,7 +53,7 @@ const DocumentIdPage = () => {
 
     return (
         <>
-        <div className="bg-[#1F1F1F] min-h-screen ">
+        <div className="bg-[#1F1F1F] min-h-screen">
                 <Cover preview url={document.coverImage} />
                 <div className="md:max-w-3xl lg:max-w-4xl mx-auto pb-auto">
                    <Toolbar preview initialData={document} />
