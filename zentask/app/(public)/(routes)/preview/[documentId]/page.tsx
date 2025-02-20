acb376fc42e7fefb9dbaa53b1e130e7faba7489d
om arora <om30arora@gmail.com>
2025-02-20 06:47:51 +0530

    preview-bg-test-6

--- zentask/app/(public)/(routes)/preview/[documentId]/page.tsx
+++ zentask/app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -53,9 +53,9 @@ const DocumentIdPage = () => {
 
     return (
         <>
-        <div className="pb-40">
+        <div className="bg-[#1F1F1F] min-h-screen ">
                 <Cover preview url={document.coverImage} />
-                <div className="md:max-w-3xl lg:max-w-4xl mx-auto dark:[#1F1F1F]">
+                <div className="md:max-w-3xl lg:max-w-4xl mx-auto pb-auto">
                    <Toolbar preview initialData={document} />
                    <Editor
                         editable={false}
