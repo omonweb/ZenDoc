d1b3687501367b415fb4e74441ddb71ccf8dce36
om arora <om30arora@gmail.com>
2025-02-20 08:07:06 +0530

    re-run-1

--- zentask/app/(marketing)/_components/logo.tsx
+++ zentask/app/(marketing)/_components/logo.tsx
@@ -30,26 +30,6 @@ export const Logo = () => {
                 ZenHub
             </p>
         </div>
-
-        <div className=" md:hidden sm:flex items-center gap-x-2">
-            <Image
-                src="/logolight.svg"
-                height="40"
-                width="40"
-                alt="Logo"
-                className="dark:hidden"
-            />
-            <Image
-                src="/logodark.svg"
-                height="40"
-                width="40"
-                alt="Logo"
-                className="hidden dark:block"
-            />
-            <p className={cn("font-semibold", font.className)}>
-                ZenHub
-            </p>
-        </div>
         </>
     )
 }
