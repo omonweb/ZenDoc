db274abd753b8fcba12b1f4641b5c7e6ba285d39
om arora <om30arora@gmail.com>
2025-02-20 07:51:30 +0530

    footer-h-test

--- zentask/app/(marketing)/_components/footer.tsx
+++ zentask/app/(marketing)/_components/footer.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./logo"
 
 export const Footer = () => {
     return (
-        <div className="flex items-center w-full p-6 bg-background  dark:bg-[#1F1F1F] z-50">
+        <div className="flex items-center w-full p-6 bg-background sm:pt-10 dark:bg-[#1F1F1F] z-50">
             <Logo />
             
         </div>
