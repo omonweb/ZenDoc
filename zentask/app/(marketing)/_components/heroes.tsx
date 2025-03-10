910006712fe5756fc5a305797303f6e3b1731e7d
Om Arora <aroraom134@gmail.com>
2025-03-10 10:38:26 +0000

    hero-2

--- zentask/app/(marketing)/_components/heroes.tsx
+++ zentask/app/(marketing)/_components/heroes.tsx
@@ -4,8 +4,8 @@ export const Heroes = () => {
     return (
         <div className="flex flex-col items-center justify-center max-w-5xl">
             <div className="flex items-center">
-                <div className="relative w-[300px] h-[300px]
-                sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                <div className="relative w-[200px] h-[200px]
+                sm:w-[250px] sm:h-[250px] md:h-[200px] md:w-[200px]">
                     <Image
                         src="/bulb.svg"
                         fill
